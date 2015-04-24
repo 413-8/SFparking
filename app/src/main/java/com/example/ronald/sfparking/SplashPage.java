0000503e1e86a4c4eea2b498f34f6d9827469107
sjnasr <sjnasr18@yahoo.com>
2015-04-24 16:38:56 -0700

    Modified AndroidManfest, added park_logo pictures, added activity_splash.xml, and added SplashPage.java

---
+++ app/src/main/java/com/example/ronald/sfparking/SplashPage.java
@@ -0,0 +1,37 @@
+package com.example.ronald.sfparking;
+
+import android.content.Context;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+/**
+ * Created by saralove on 4/22/2015.
+ */
+public class SplashPage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        final Context sp = this;
+        Thread background = new Thread() {
+            public void run() {
+
+                try {
+                    // Thread will sleep for 5 seconds
+                    sleep(5000);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }finally{
+                    Intent open = new Intent(sp, MapsActivity.class);
+                    startActivity(open);
+                }
+            }
+        };
+        background.start();
+    }
+    protected void onPause(){
+        super.onPause();
+        finish();
+    }
+}
