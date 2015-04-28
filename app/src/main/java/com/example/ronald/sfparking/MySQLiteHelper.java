6c81d4cda552003b62b09aaa27178edd5130ce50
sam-sfsu <scandito@mail.sfsu.edu>
2015-04-28 07:53:45 -0700

    Updates MySQLiteHelper.java to implement SqliteSchema.java.

--- app/src/main/java/com/example/ronald/sfparking/MySQLiteHelper.java
+++ app/src/main/java/com/example/ronald/sfparking/MySQLiteHelper.java
@@ -6,44 +6,29 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
 /**
- * Created by KingRat on 4/26/2015.
+ * Helper for creating and upgrading local SQLite database "Parking_Information.db". Uses string
+ * constructors from SqliteSchema to create and upgrade (clear and recreate) the database.
  */
 public class MySQLiteHelper extends SQLiteOpenHelper {
 
     public static final int DATABASE_VERSON = 1;
     public static final String DATABASE_NAME = "Parking_Information.db";
-    public static final String TABLE_PARKINFO = "parking_info";
 
-    public static final String KEY_ID = "_id";
-
-    public static final String KEY_LONGITUDE = "Longitude",
-        KEY_LATITUDE = "Latitude",
-        KEY_TIME = "Time",
-        KEY_STREET_NAME = "Street_name",
-        KEY_ON_OFF_STREET = "on_off_Street";
-
-    public static final String DATABASE_CREATE = "CREATE TABLE "
-            + TABLE_PARKINFO + "("
-            + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-            + KEY_LONGITUDE + " TEXT,"
-            + KEY_LATITUDE + " TEXT,"
-            + KEY_TIME + " TEXT,"
-            + KEY_STREET_NAME + " TEXT,"
-            + KEY_ON_OFF_STREET + " TEXT" + ");";
-    public MySQLiteHelper(Context context){
+    public MySQLiteHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSON);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase database){database.execSQL(DATABASE_CREATE);}
+    public void onCreate(SQLiteDatabase database) {
+        database.execSQL(SqliteSchema.CREATE_TABLE);
+    }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-        Log.d(MySQLiteHelper.class.getName(),
-                "Upgrading database from version " + oldVersion + " to "
-                        + newVersion + ", which will destroy all old data");
-        db.execSQL("DROP TABLE IF EXISTS + TABLE_PARKINFO");
-        onCreate(db);
+    public void onUpgrade(SQLiteDatabase database, int oldVer, int newVer){
+        Log.d(MySQLiteHelper.class.getName(), "Upgrading database from version " + oldVer + " to " +
+                newVer + ", which will destroy all old data");
+        database.execSQL(SqliteSchema.DELETE_TABLE);
+        onCreate(database);
     }
 
 }
