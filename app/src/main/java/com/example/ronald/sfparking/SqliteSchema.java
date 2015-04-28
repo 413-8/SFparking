1d28dff14e1a931409fdafa8db16e8205b59fc7b
sam-sfsu <scandito@mail.sfsu.edu>
2015-04-28 11:54:35 -0700

    Fixes data types of some info fields in the SQL command string constructor (see changes).

--- app/src/main/java/com/example/ronald/sfparking/SqliteSchema.java
+++ app/src/main/java/com/example/ronald/sfparking/SqliteSchema.java
@@ -18,18 +18,18 @@ public final class SqliteSchema {
         public static final String COLUMN_ID = "id";
         public static final String COLUMN_LONGITUDE = "longitude";
         public static final String COLUMN_LATITUDE = "latitude";
-        public static final String COLUMN_TIME = "time";
         public static final String COLUMN_STREET = "street";
         public static final String COLUMN_ON_OFF = "on_off";
+        public static final String COLUMN_TIME = "time";
     }
 
     protected static final String CREATE_TABLE = "CREATE TABLE" + SqlEntry.TABLE_NAME + " (" +
             SqlEntry.COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
-            SqlEntry.COLUMN_LONGITUDE + " REAL, " +
-            SqlEntry.COLUMN_LATITUDE + " REAL, " +
+            SqlEntry.COLUMN_LONGITUDE + " TEXT, " +
+            SqlEntry.COLUMN_LATITUDE + " TEXT, " +
+            SqlEntry.COLUMN_STREET + " TEXT, " +
+            SqlEntry.COLUMN_ON_OFF + " TEXT" +
             SqlEntry.COLUMN_TIME + " NUMERIC, " +
-            SqlEntry.COLUMN_STREET + " STRING, " +
-            SqlEntry.COLUMN_ON_OFF + " STRING" +
             " )";
 
     protected static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + SqlEntry.TABLE_NAME;
