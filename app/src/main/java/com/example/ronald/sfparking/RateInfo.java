867047daad4cdd83bd7f196a2a8a689033de9113
Pedro Garate <pedro.garate@yahoo.com>
2015-05-03 02:48:06 -0700

    Created RateInfo.java A RateInfo object gives information about meter rate for a determined period of time. It has beginning and ending times, a rate and request variables. The toString() method has been overriden to give a specific output.

---
+++ app/src/main/java/com/example/ronald/sfparking/RateInfo.java
@@ -0,0 +1,72 @@
+package com.example.ronald.sfparking;
+
+/**
+ * Created by Pedro on 5/2/2015.
+ */
+public class RateInfo {
+    private String beg;
+    private String end;
+    private String rate;
+    private String rq;
+
+    public RateInfo() {}
+
+    // getters
+    public String getBeg() {
+        return beg;
+    }
+
+    public String getEnd() {
+        return end;
+    }
+
+    public String getRate() {
+        return rate;
+    }
+
+    public String getRq() {
+        return rq;
+    }
+
+    // setters
+    public void setBeg(String start) {
+        this.beg = start;
+    }
+
+    public void setEnd(String finish) {
+        this.end = finish;
+    }
+
+    public void setRate(String money) {
+        this.rate = money;
+    }
+
+    public void setRq(String request) {
+        this.rq = request;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder temp = new StringBuilder();
+        temp.append("\t\t");
+        temp.append(beg);
+        temp.append(" - ");
+        temp.append(end);
+        temp.append(":\n");
+
+        if(rate.equals("0")) {
+            temp.append("\t\t\t");
+            temp.append(rq);
+            temp.append("\n");
+        }
+        else {
+            temp.append("\t\t\t");
+            temp.append(rate);
+            temp.append(" ");
+            temp.append(rq);
+            temp.append("\n");
+        }
+
+        return temp.toString();
+    }
+}
