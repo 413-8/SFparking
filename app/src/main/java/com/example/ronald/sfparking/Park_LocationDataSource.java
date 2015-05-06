5a5843efad289c5af83777488dd12f77e0f8afbf
Karl <hypnothistle@yahoo.com>
2015-05-05 17:05:24 -0700

    Update Park_LocationDataSource.java
    added documentation, altered the createLocationInfo and getAllLocations methods so that the database only has 15 rows that it uses, in order to conserve space used by the app.
    made an updateLocationInfoRow method to facilitate the methods.

--- app/src/main/java/com/example/ronald/sfparking/Park_LocationDataSource.java
+++ app/src/main/java/com/example/ronald/sfparking/Park_LocationDataSource.java
@@ -6,6 +6,8 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.util.Log;
 import android.view.View;
 
 import java.util.Stack;
@@ -14,13 +16,14 @@ import static com.example.ronald.sfparking.SqliteSchema.*;
 
 /**
  * Created by KingRat on 4/26/2015.
+ * Interacts with the database on the device. All CRUD methods are defined here for tables in the
+ * database.
  */
 public class Park_LocationDataSource {
-
+        private static int oldestID =0;
         private SQLiteDatabase database;
         private MySQLiteHelper dbHelper;
         private String [] allColumns = getColumns();
-
         public Park_LocationDataSource(Context context){dbHelper = MySQLiteHelper.getInstance(context);}
 
         public void read() {database = dbHelper.getReadableDatabase();}
@@ -33,37 +36,77 @@ public class Park_LocationDataSource {
 
         public void close() {dbHelper.close();}
 
+    /**
+     * takes information from a LocationInfo object and stores it as a new row in the table.
+     * maximum number of stored entries:15
+     * @param locationinfo the LocationInfo object to be added to the table in a new row.
+     */
         public void createLocationInfo(LocationInfo locationinfo){
-
             ContentValues values = new ContentValues();
             values.put(SqlEntry.COLUMN_LONGITUDE, locationinfo.getLongitude());
             values.put(SqlEntry.COLUMN_LATITUDE, locationinfo.getLatitude());
             values.put(SqlEntry.COLUMN_STREET, locationinfo.getStreetname());
             values.put(SqlEntry.COLUMN_ON_OFF, locationinfo.getOn_off_street());
             values.put(SqlEntry.COLUMN_TIME, locationinfo.getTime());
-
-            long insertId = database.insert(SqlEntry.TABLE_NAME, null, values);
             Cursor cursor = database.query(
                     SqlEntry.TABLE_NAME,
                     allColumns,
-                    SqlEntry.COLUMN_ID + " = " + insertId,
+                    null,
                     null,
                     null,
                     null,
                     null
             );
-            cursor.moveToNext();
+            if(cursor.getCount()>14) {
+                updateLocationInfoRow(locationinfo);
+                oldestID=(oldestID+1)%15;
+            }else{
+                long insertId = database.insert(SqlEntry.TABLE_NAME, null, values);
+            }
+
 
         }
+
+
+    /**
+     * Updates a row of the SQLite table with a new LocationInfo object
+     * @param newLoc The new LocationInfo object to update the row.
+     */
+    public void updateLocationInfoRow( LocationInfo newLoc){
+        //UPDATE TABLE_NAME SET longitude=newLoc.longitude WHERE id=
+        database.rawQuery("UPDATE " +SqlEntry.TABLE_NAME + " SET " +SqlEntry.COLUMN_LONGITUDE + "="
+                + newLoc.getLongitude()+" WHERE " + SqlEntry.COLUMN_ID + "="
+                + oldestID,null);
+        database.rawQuery("UPDATE " +SqlEntry.TABLE_NAME + " SET " + SqlEntry.COLUMN_LATITUDE + "="
+                + newLoc.getLatitude()+" WHERE " + SqlEntry.COLUMN_ID + "="
+                + oldestID,null);
+        database.rawQuery("UPDATE " +SqlEntry.TABLE_NAME + " SET " + SqlEntry.COLUMN_STREET + "='"
+                + newLoc.getStreetname()+"' WHERE " + SqlEntry.COLUMN_ID + "="
+                + oldestID,null);
+        database.rawQuery("UPDATE " +SqlEntry.TABLE_NAME + " SET " + SqlEntry.COLUMN_ON_OFF + "='"
+                + newLoc.getOn_off_street()+"' WHERE " + SqlEntry.COLUMN_ID + "="
+                + oldestID,null);
+        database.rawQuery("UPDATE " +SqlEntry.TABLE_NAME + " SET " + SqlEntry.COLUMN_TIME + "='"
+                + newLoc.getTime()+"' WHERE " + SqlEntry.COLUMN_ID + "="
+                + oldestID,null);
+
+    }
+
+    /**
+     * Retrieves a LocationInfo object from the history table.
+     * @param id the id of the LocationInfo object requested
+     * @return the LocationInfo Object requested from the table.
+     */
         public LocationInfo getLocationInfo(int id){
             Cursor cursor = database.query(
                     SqlEntry.TABLE_NAME,
                     allColumns,
-                    SqlEntry.COLUMN_ID + " = " + id,
+                    SqlEntry.COLUMN_ID + "=" + id,
                     null,
                     null,
                     null,
                     null
+
             );
             cursor.moveToFirst();
             LocationInfo location = new LocationInfo();
@@ -71,15 +114,30 @@ public class Park_LocationDataSource {
             cursor.close();
             return location;
         }
+
+    /**
+     * deletes the LocationInfo object from the history table
+     * @param locationinfo the locationInfo Object to be deleted.
+     */
         public void deleteLocationInfo(LocationInfo locationinfo){
             long id = locationinfo.getId();
             database.delete(SqlEntry.TABLE_NAME, SqlEntry.COLUMN_ID + " = " + id, null);
             database.close();
         }
 
+    /**
+     * gets all locations in the database table as a stack with the newest entry on top.
+     * @return a stack of all of the locations in the database history table.
+     */
         public Stack<LocationInfo> getAllLocations() {
+            /*
+            starts by pointing at oldest entry and adding it to stack.
+            then loops, adding the LocationInfo objects, checking id and breaking if it encounters
+            the oldest entry's id again.
+             */
+            LocationInfo location;
             Stack<LocationInfo> locations = new Stack<LocationInfo>();
-
+            String seeker = SqlEntry.COLUMN_ID + " = " + oldestID;
             Cursor cursor = database.query(
                     SqlEntry.TABLE_NAME,
                     allColumns,
@@ -91,12 +149,32 @@ public class Park_LocationDataSource {
             );
             cursor.moveToFirst();
             if (cursor.isNull(0)) {
+               return locations;
+            }
+            if(oldestID==0)
+            {
+                while(!cursor.isAfterLast())
+                {
+                    location = cursorToLocationInfo(cursor);
+                    locations.push(location);
+                    cursor.moveToNext();
+                }
+                cursor.close();
                 return locations;
             }
-            while (!cursor.isAfterLast()) {
-                LocationInfo location = cursorToLocationInfo(cursor);
+            cursor.move(oldestID);
+            location = cursorToLocationInfo(cursor);
+            locations.push(location);
+            cursor.moveToNext();
+            if(cursor.isAfterLast())
+                cursor.moveToFirst();
+            while (cursor.getInt(cursor.getColumnIndex(SqlEntry.COLUMN_ID))!=oldestID) {
+                location = cursorToLocationInfo(cursor);
                 locations.push(location);
                 cursor.moveToNext();
+                if(cursor.isAfterLast()){
+                    cursor.moveToFirst();
+                }
             }
             cursor.close();
             return locations;
