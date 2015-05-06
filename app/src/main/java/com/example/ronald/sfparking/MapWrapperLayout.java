f7d85e44d9dcaf241920052d255fca92d2ba698f
torenshahar <tsoib@icloud.com>
2015-05-06 00:43:37 -0700

    New helper classes that create a wrapper for the map so we can handle drag events.

---
+++ app/src/main/java/com/example/ronald/sfparking/MapWrapperLayout.java
@@ -0,0 +1,43 @@
+package com.example.ronald.sfparking;
+
+/**
+ * Created by toren on 5/5/15.
+ */
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import com.google.android.gms.maps.SupportMapFragment;
+
+/**
+ * MapWrapperLayout handles drag events form the user onto the map
+ */
+public class MapWrapperLayout extends FrameLayout {
+
+    public interface OnDragListener {
+        public void onDrag(MotionEvent motionEvent);
+    }
+
+    private OnDragListener mOnDragListener;
+
+    public MapWrapperLayout(Context context) {
+        super(context);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (mOnDragListener != null) {
+            mOnDragListener.onDrag(ev);
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public void setOnDragListener(OnDragListener mOnDragListener) {
+        this.mOnDragListener = mOnDragListener;
+    }
+}
