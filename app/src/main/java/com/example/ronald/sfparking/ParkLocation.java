1a35995ddefdb477638b934b013c2e8bd4c81ca9
Pedro Garate <pedro.garate@yahoo.com>
2015-05-11 02:09:06 -0700

    Update on ParkLocation.java: -Added strings variables to handle address and phone number of a garage. -Added a new constructor to handle a garage ParkLocation. -Updated getOnOffStreet to show either "Meter" or "Garage" instead of "ON" or "OFF". -Added getters and setters for address and phone variables. -Updated javadoc comments.

--- app/src/main/java/com/example/ronald/sfparking/ParkLocation.java
+++ app/src/main/java/com/example/ronald/sfparking/ParkLocation.java
@@ -11,15 +11,40 @@ import java.util.ArrayList;
  */
 public class ParkLocation {
     private String onOffStreet = "No Data";
-    private String streetName = "No Data";
+    private String name = "No Data";
     private ArrayList<RateInfo> rates;
+    private String address = "";
+    private String phone = "";
 
     public ParkLocation () {}
 
+    /**
+     * Constructor for meters
+     * @param onOffStreet Type of parking
+     * @param stName Street name
+     * @param rates Rates
+     */
     public ParkLocation(String onOffStreet, String stName, ArrayList<RateInfo> rates) {
         this.onOffStreet = onOffStreet;
-        this.streetName = stName;
+        this.name = stName;
         this.rates = rates;
+    }
+
+    /**
+     * Constructor for garages
+     * @param onOffStreet Type of parking
+     * @param garageName Garage name
+     * @param rates Rates
+     * @param garageAddress Garage address
+     * @param garagePhone Garage phone number
+     */
+    public ParkLocation(String onOffStreet, String garageName, ArrayList<RateInfo> rates,
+                        String garageAddress, String garagePhone) {
+        this.onOffStreet = onOffStreet;
+        this.name = garageName;
+        this.rates = rates;
+        this.address = garageAddress;
+        this.phone = garagePhone;
     }
 
     /**
@@ -27,15 +52,38 @@ public class ParkLocation {
      * @return the on/off street status of the parking for the ParkLocation object.
      */
     public String getOnOffStreet(){
-        return onOffStreet;
+        String result = "";
+        if(onOffStreet.equals("ON")) {
+            result = "Meter";
+        }
+        else if (onOffStreet.equals("OFF")) {
+            result = "Garage";
+        }
+        return result;
     }
 
     /**
      *
-     * @return the street name held by the ParkLocation object.
+     * @return the name held by the ParkLocation object.
      */
-    public String getStreetName(){
-        return streetName;
+    public String getName(){
+        return name;
+    }
+
+    /**
+     *
+     * @return the address held by the ParkLocation object if it is a Garate
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     *
+     * @return the phone number held by the ParkLocation object if it is a Garage
+     */
+    public String getPhone() {
+        return phone;
     }
 
     /**
