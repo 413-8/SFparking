f16cbcecc294a5606548b05e3b18e4be10890206
torenshahar <tsoib@icloud.com>
2015-05-12 20:55:45 -0700

    Added method `isEmpty()` that is called in MapsActivity to determine the state of the History Button. (copied from ParkDbAccessor) Reformat Code.

--- app/src/main/java/com/example/ronald/sfparking/SavedDbAccessor.java
+++ app/src/main/java/com/example/ronald/sfparking/SavedDbAccessor.java
@@ -17,65 +17,76 @@ import static com.example.ronald.sfparking.SqliteSchema.getColumns;
  * tables in the database.
  */
 public class SavedDbAccessor {
-        private static int oldestID =1;
-        private SQLiteDatabase database;
-        private SavedDbHelper dbHelper;
-        private String [] allColumns = getColumns();
-        public SavedDbAccessor(Context context){dbHelper = SavedDbHelper.getInstance(context);}
+    private static int oldestID = 1;
+    private SQLiteDatabase database;
+    private SavedDbHelper dbHelper;
+    private String[] allColumns = getColumns();
 
-        public void read() {database = dbHelper.getReadableDatabase();}
+    public SavedDbAccessor(Context context) {
+        dbHelper = SavedDbHelper.getInstance(context);
+    }
 
-        public void write() {database = dbHelper.getWritableDatabase();}
+    public void read() {
+        database = dbHelper.getReadableDatabase();
+    }
 
-        public void open() throws SQLException {
-            database = dbHelper.getWritableDatabase();
-        }
+    public void write() {
+        database = dbHelper.getWritableDatabase();
+    }
 
-        public void close() {dbHelper.close();}
+    public void open() throws SQLException {
+        database = dbHelper.getWritableDatabase();
+    }
+
+    public void close() {
+        dbHelper.close();
+    }
 
     /**
      * takes information from a ParkLocationInfo object and stores it as a new row in the table.
      * maximum number of stored entries:5
+     *
      * @param locationinfo the ParkLocationInfo object to be added to the table in a new row.
      */
-        public void createLocationInfo(ParkLocationInfo locationinfo){
-            ContentValues values = new ContentValues();
-            values.put(SqlEntry.COLUMN_LONGITUDE, locationinfo.getLongitude());
-            values.put(SqlEntry.COLUMN_LATITUDE, locationinfo.getLatitude());
-            values.put(SqlEntry.COLUMN_STREET, locationinfo.getStreetName());
-            values.put(SqlEntry.COLUMN_ON_OFF, locationinfo.getOnOffStreet());
-            values.put(SqlEntry.COLUMN_TIME, locationinfo.getTime());
-            Cursor cursor = database.query(
-                    SqlEntry.TABLE_NAME,
-                    allColumns,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
-            );
-            if(cursor.getCount()>4) {
-                database.update(SqlEntry.TABLE_NAME, values, SqlEntry.COLUMN_ID + " = " + oldestID, null);
-                oldestID=(oldestID+1)%6;
-                System.out.print(oldestID);
-                if(oldestID==0)
-                    oldestID =1;
-                cursor.close();
-            }else{
-                long insertId = database.insert(SqlEntry.TABLE_NAME, null, values);
-                cursor.close();
-            }
-
-
+    public void createLocationInfo(ParkLocationInfo locationinfo) {
+        ContentValues values = new ContentValues();
+        values.put(SqlEntry.COLUMN_LONGITUDE, locationinfo.getLongitude());
+        values.put(SqlEntry.COLUMN_LATITUDE, locationinfo.getLatitude());
+        values.put(SqlEntry.COLUMN_STREET, locationinfo.getStreetName());
+        values.put(SqlEntry.COLUMN_ON_OFF, locationinfo.getOnOffStreet());
+        values.put(SqlEntry.COLUMN_TIME, locationinfo.getTime());
+        Cursor cursor = database.query(
+                SqlEntry.TABLE_NAME,
+                allColumns,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        if (cursor.getCount() > 4) {
+            database.update(SqlEntry.TABLE_NAME, values, SqlEntry.COLUMN_ID + " = " + oldestID, null);
+            oldestID = (oldestID + 1) % 6;
+            System.out.print(oldestID);
+            if (oldestID == 0)
+                oldestID = 1;
+            cursor.close();
+        } else {
+            long insertId = database.insert(SqlEntry.TABLE_NAME, null, values);
+            cursor.close();
         }
+
+
+    }
 
 
     /**
      * Updates a specified row of the SQLite table with a new ParkLocationInfo object.
      * use database.update if you want to update by using the id to find the value to replace.
+     *
      * @param newLoc The new ParkLocationInfo object to update the row.
      */
-    public void updateLocationInfoRow( ParkLocationInfo newLoc, int row){
+    public void updateLocationInfoRow(ParkLocationInfo newLoc, int row) {
         //UPDATE TABLE_NAME SET longitude=newLoc.longitude WHERE id=
         ContentValues values = new ContentValues();
         values.put(SqlEntry.COLUMN_LONGITUDE, newLoc.getLongitude());
@@ -88,110 +99,137 @@ public class SavedDbAccessor {
 
     /**
      * Retrieves a ParkLocationInfo object from the history table.
+     *
      * @param id the id of the ParkLocationInfo object requested
      * @return the ParkLocationInfo Object requested from the table.
      */
-        public ParkLocationInfo getLocationInfo(int id){
-            Cursor cursor = database.query(
-                    SqlEntry.TABLE_NAME,
-                    allColumns,
-                    SqlEntry.COLUMN_ID + "=" + id,
-                    null,
-                    null,
-                    null,
-                    null
+    public ParkLocationInfo getLocationInfo(int id) {
+        Cursor cursor = database.query(
+                SqlEntry.TABLE_NAME,
+                allColumns,
+                SqlEntry.COLUMN_ID + "=" + id,
+                null,
+                null,
+                null,
+                null
 
-            );
-            cursor.moveToFirst();
-            ParkLocationInfo location = new ParkLocationInfo();
-            location = cursorToLocationInfo(cursor);
-            cursor.close();
-            return location;
-        }
+        );
+        cursor.moveToFirst();
+        ParkLocationInfo location = new ParkLocationInfo();
+        location = cursorToLocationInfo(cursor);
+        cursor.close();
+        return location;
+    }
 
     /**
      * deletes the ParkLocationInfo object from the history table
+     *
      * @param locationinfo the locationInfo Object to be deleted.
      */
-        public void deleteLocationInfo(ParkLocationInfo locationinfo){
-            long id = locationinfo.getId();
-            database.delete(SqlEntry.TABLE_NAME, SqlEntry.COLUMN_ID + " = " + id, null);
-            database.close();
-        }
+    public void deleteLocationInfo(ParkLocationInfo locationinfo) {
+        long id = locationinfo.getId();
+        database.delete(SqlEntry.TABLE_NAME, SqlEntry.COLUMN_ID + " = " + id, null);
+        database.close();
+    }
 
     /**
      * gets all locations in the database table as a stack with the newest entry on top.
+     *
      * @return a stack of all of the locations in the database history table.
      */
-        public Stack<ParkLocationInfo> getAllLocations() {
+    public Stack<ParkLocationInfo> getAllLocations() {
             /*
             starts by pointing at oldest entry and adding it to stack.
             then loops, adding the ParkLocationInfo objects, checking id and breaking if it encounters
             the oldest entry's id again.
              */
-            ParkLocationInfo location;
-            Stack<ParkLocationInfo> locations = new Stack<ParkLocationInfo>();
-            String seeker = SqlEntry.COLUMN_ID + " = " + oldestID;
-            Cursor cursor = database.query(
-                    SqlEntry.TABLE_NAME,
-                    allColumns,
-                    null,
-                    null,
-                    null,
-                    null,
-                    null
-            );
-            cursor.moveToFirst();
-            if (cursor.isNull(0)) {
-               return locations;
-            }
-            if(oldestID==1)
-            {
-                while(!cursor.isAfterLast())
-                {
-                    location = cursorToLocationInfo(cursor);
-                    locations.push(location);
-                    cursor.moveToNext();
-                }
-                cursor.close();
-                return locations;
-            }
-            cursor.move((oldestID-1));
-            location = cursorToLocationInfo(cursor);
-            locations.push(location);
-            cursor.moveToNext();
-            if(cursor.isAfterLast())
-                cursor.moveToFirst();
-            while (cursor.getInt(cursor.getColumnIndex(SqlEntry.COLUMN_ID))!=oldestID) {
+        ParkLocationInfo location;
+        Stack<ParkLocationInfo> locations = new Stack<ParkLocationInfo>();
+        String seeker = SqlEntry.COLUMN_ID + " = " + oldestID;
+        Cursor cursor = database.query(
+                SqlEntry.TABLE_NAME,
+                allColumns,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+        cursor.moveToFirst();
+        if (cursor.isNull(0)) {
+            return locations;
+        }
+        if (oldestID == 1) {
+            while (!cursor.isAfterLast()) {
                 location = cursorToLocationInfo(cursor);
                 locations.push(location);
                 cursor.moveToNext();
-                if(cursor.isAfterLast()){
-                    cursor.moveToFirst();
-                }
             }
             cursor.close();
             return locations;
         }
-
-        /**
-         * Takes a Cursor object pointing to a row of the locations database and parses the data into
-         * a new ParkLocationInfo object, which is returned on completion.
-         * @param cursor Cursor object pointing to row containing ParkLocationInfo data
-         * @return ParkLocationInfo object containing row data pointed to by cursor
-         */
-        private ParkLocationInfo cursorToLocationInfo(Cursor cursor) {
-            ParkLocationInfo location = new ParkLocationInfo();
-
-            location.setId( cursor.getInt(cursor.getColumnIndex(SqlEntry.COLUMN_ID)) );
-            location.setLongitude( cursor.getDouble(cursor.getColumnIndex(SqlEntry.COLUMN_LONGITUDE)) );
-            location.setLatitude( cursor.getDouble(cursor.getColumnIndex(SqlEntry.COLUMN_LATITUDE)) );
-            location.setStreetName(cursor.getString(cursor.getColumnIndex(SqlEntry.COLUMN_STREET)));
-            location.setOnOffStreet(cursor.getString(cursor.getColumnIndex(SqlEntry.COLUMN_ON_OFF)));
-            location.setTime( cursor.getString(cursor.getColumnIndex(SqlEntry.COLUMN_TIME)) );
-
-            return location;
+        cursor.move((oldestID - 1));
+        location = cursorToLocationInfo(cursor);
+        locations.push(location);
+        cursor.moveToNext();
+        if (cursor.isAfterLast())
+            cursor.moveToFirst();
+        while (cursor.getInt(cursor.getColumnIndex(SqlEntry.COLUMN_ID)) != oldestID) {
+            location = cursorToLocationInfo(cursor);
+            locations.push(location);
+            cursor.moveToNext();
+            if (cursor.isAfterLast()) {
+                cursor.moveToFirst();
+            }
         }
+        cursor.close();
+        return locations;
+    }
+
+    /**
+     * Checks if the saved locations database is currently empty by using the Cursor object
+     * method moveToFirst(), which returns a boolean value depending on what the Cursor object
+     * is pointing to (false if Cursor object is empty).
+     *
+     * @return true if database is empty, false if not.
+     */
+    public boolean isEmpty() {
+        Cursor cursor = database.query(
+                SqlEntry.TABLE_NAME,
+                allColumns,
+                null,
+                null,
+                null,
+                null,
+                null
+        );
+
+        // cursor.moveToFirst() returns false if cursor is pointing to an empty array.
+        if (cursor.moveToFirst())
+            return false;
+        else
+            return true;
+    }
+
+    /**
+     * Takes a Cursor object pointing to a row of the locations database and parses the data into
+     * a new ParkLocationInfo object, which is returned on completion.
+     *
+     * @param cursor Cursor object pointing to row containing ParkLocationInfo data
+     * @return ParkLocationInfo object containing row data pointed to by cursor
+     */
+    private ParkLocationInfo cursorToLocationInfo(Cursor cursor) {
+        ParkLocationInfo location = new ParkLocationInfo();
+
+        location.setId(cursor.getInt(cursor.getColumnIndex(SqlEntry.COLUMN_ID)));
+        location.setLongitude(cursor.getDouble(cursor.getColumnIndex(SqlEntry.COLUMN_LONGITUDE)));
+        location.setLatitude(cursor.getDouble(cursor.getColumnIndex(SqlEntry.COLUMN_LATITUDE)));
+        location.setStreetName(cursor.getString(cursor.getColumnIndex(SqlEntry.COLUMN_STREET)));
+        location.setOnOffStreet(cursor.getString(cursor.getColumnIndex(SqlEntry.COLUMN_ON_OFF)));
+        location.setTime(cursor.getString(cursor.getColumnIndex(SqlEntry.COLUMN_TIME)));
+
+        return location;
+    }
 
 
 }
